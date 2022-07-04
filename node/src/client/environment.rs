76855b265414355fccbfabec807296a818008d96
qhz-ty <1590398762@qq.com>
2022-07-04 13:09:03 +0800

    fix compilation error on windows

--- node/src/client/environment.rs
+++ node/src/client/environment.rs
@@ -180,14 +180,10 @@ impl Environment {
         // setup for handling a Ctrl-C
         let (ctrlc_send, ctrlc_oneshot) = oneshot::channel();
         let ctrlc_send_c = RefCell::new(Some(ctrlc_send));
-        let log = self.log.clone();
         ctrlc::set_handler(move || {
             if let Some(ctrlc_send) = ctrlc_send_c.try_borrow_mut().unwrap().take() {
                 if let Err(e) = ctrlc_send.send(()) {
-                    error!(
-                        error = %e,
-                        "Error sending ctrl-c message",
-                    );
+                    error!("Error sending ctrl-c message: {:?}", e);
                 }
             }
         })
