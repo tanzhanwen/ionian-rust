14e387cb5591ffbdd4a5df7af2b9adaab49a5fe4
qhz-ty <105765463+qhz-ty@users.noreply.github.com>
2022-08-01 15:47:43 +0800

    add expiry for file location cache (#55)
    * add expiry for file location cache
    
    * update expiration time

--- node/gossip_cache/src/gossip_cache.rs
+++ node/gossip_cache/src/gossip_cache.rs
@@ -1,32 +1,61 @@
+use crate::Config;
 use hashlink::{LinkedHashMap, LinkedHashSet};
 use network::types::SignedAnnounceFile;
 use parking_lot::Mutex;
 use rand::seq::IteratorRandom;
+use std::ops::{Add, DerefMut};
+use std::time::Instant;
 
-const MAX_FILES: usize = 1000;
-const MAX_ENTRIES_PER_FILE: usize = 3;
+struct CachedFiles {
+    expired_at: Instant,
+    /// TODO(ionian-dev): using `LinkedHashSet` as an entry is probably an overkill
+    files: LinkedHashSet<SignedAnnounceFile>,
+}
 
 #[derive(Default)]
 pub struct GossipCache {
-    /// TODO(ionian-dev): using `LinkedHashSet` as an entry is probably an overkill
-    cache: Mutex<LinkedHashMap<u64, LinkedHashSet<SignedAnnounceFile>>>,
+    config: Config,
+    cache: Mutex<LinkedHashMap<u64, CachedFiles>>,
 }
 
 impl GossipCache {
+    fn garbage_collect(&self, cache: &mut LinkedHashMap<u64, CachedFiles>) {
+        let now = Instant::now();
+
+        while let Some((_, files)) = cache.front() {
+            if files.expired_at > now {
+                return;
+            }
+
+            if let Some((tx_seq, _)) = cache.pop_front() {
+                debug!(%tx_seq, "Garbage collected for file");
+            }
+        }
+    }
+
+    fn expiration_time(&self) -> Instant {
+        Instant::now().add(self.config.entry_expiration_time)
+    }
+
     pub fn insert(&self, announcement: SignedAnnounceFile) {
         let mut cache = self.cache.lock();
 
+        self.garbage_collect(cache.deref_mut());
+
         let entry = cache
             .entry(announcement.tx_seq)
-            .or_insert_with(Default::default);
+            .or_insert_with(|| CachedFiles {
+                expired_at: self.expiration_time(),
+                files: Default::default(),
+            });
 
-        entry.insert(announcement);
+        entry.files.insert(announcement);
 
-        if entry.len() > MAX_ENTRIES_PER_FILE {
-            entry.pop_front();
+        if entry.files.len() > self.config.max_entries_per_file {
+            entry.files.pop_front();
         }
 
-        if cache.len() > MAX_FILES {
+        if cache.len() > self.config.max_files {
             cache.pop_front();
         }
     }
@@ -34,18 +63,24 @@ impl GossipCache {
     pub fn get_one(&self, tx_seq: u64) -> Option<SignedAnnounceFile> {
         let mut cache = self.cache.lock();
 
-        cache
-            .to_back(&tx_seq)
-            .and_then(|a| a.iter().choose(&mut rand::thread_rng()))
-            .cloned()
+        self.garbage_collect(cache.deref_mut());
+
+        let cached = cache.to_back(&tx_seq)?;
+        cached.expired_at = self.expiration_time();
+        cached.files.iter().choose(&mut rand::thread_rng()).cloned()
     }
 
     pub fn get_all(&self, tx_seq: u64) -> Vec<SignedAnnounceFile> {
         let mut cache = self.cache.lock();
 
-        match cache.to_back(&tx_seq) {
-            Some(entries) => entries.iter().cloned().collect(),
-            None => vec![],
-        }
+        self.garbage_collect(cache.deref_mut());
+
+        let cached = match cache.to_back(&tx_seq) {
+            Some(entries) => entries,
+            None => return vec![],
+        };
+
+        cached.expired_at = self.expiration_time();
+        cached.files.iter().cloned().collect()
     }
 }
